586b0067d66c121303f7dbd970f826d31ba7425e
Frederic Guyont <you@example.com>
2024-02-14 17:42:55 +0100

    ADD: register e2e tests

---
+++ front/cypress/e2e/register.cy.ts
@@ -0,0 +1,31 @@
+describe('Register spec', () => {
+  it('Register successfull', () => {
+    cy.visit('/register')
+
+    cy.intercept('POST', '/api/auth/register', {
+      statusCode: 200
+    })
+
+    cy.get('input[formControlName=firstName]').type("Serious")
+    cy.get('input[formControlName=lastName]').type("Lee")
+    cy.get('input[formControlName=email]').type("serious-lee@mail.com")
+    cy.get('input[formControlName=password]').type(`${"lee!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/login')
+  })
+
+  it('Register failed with an invalid password with one character and error message appeared', () => {
+    cy.visit('/register')
+
+    cy.intercept('POST', '/api/auth/register', {
+      statusCode: 400
+    })
+
+    cy.get('input[formControlName=firstName]').type("Serious")
+    cy.get('input[formControlName=lastName]').type("Lee")
+    cy.get('input[formControlName=email]').type("serious-lee@mail.com")
+    cy.get('input[formControlName=password]').type(`${"l"}{enter}{enter}`)
+
+    cy.get('form > span').contains("error")
+  })
+});
