deaa25215093506ae428b13dc75a0ed6d3018c4b
Frederic Guyont <you@example.com>
2024-03-03 18:41:06 +0100

    ADD: session detail, session form e2e

--- front/cypress/e2e/all-tests.cy.ts
+++ front/cypress/e2e/all-tests.cy.ts
@@ -162,3 +162,95 @@ describe('sessions', ()=>{
       cy.contains('Rentals');
   })
 });
+
+describe('session detail', () => {
+  it('shows session detail', () => {
+    cy.visit('/login');
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept('GET', '/api/session', {
+      body: [{
+        id: 1,
+        name: "Beginner test session",
+        teacher_id: 1,
+        description: "Session for test"
+      }]
+    });
+
+    cy.intercept('GET', '/api/teacher', {
+      body: [{
+        id: 1,
+        lastName: "Delahaye",
+        firstName: "Margot"
+      }]
+    });
+
+    cy.get('input[formControlName="email"]').type('yoga@studio.com');
+    cy.get('input[formControlName="password"]').type('test!1234');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', 'sessions');
+
+    cy.contains('Rentals');
+
+    let session = {
+      id: 1,
+      name: "Beginner test session",
+      teacher_id: 1,
+      description: "Session for test"
+    };
+    let teacher = {
+      id: 1,
+      lastName: "DELAHAYE",
+      firstName: "Margot"
+    };
+
+    cy.intercept('GET', '/api/session/1', session);
+    cy.intercept('GET', '/api/teacher/1', teacher);
+
+    cy.get('button[mat-raised-button]').contains('Detail').click();
+
+    cy.url().should('include', 'detail');
+  })
+});
+
+describe('session form', () => {
+  it('shows session creation form', () => {
+    cy.visit('/login');
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept('GET', '/api/session', {
+      body: [{
+        id: 1,
+        name: "Beginner test session",
+        teacher_id: 1,
+        description: "Session for test"
+      }]
+    });
+
+    cy.get('input[formControlName="email"]').type('yoga@studio.com');
+    cy.get('input[formControlName="password"]').type('test!1234');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', 'sessions');
+
+    cy.contains('Rentals');
+
+    cy.get('button[mat-raised-button]').contains('Create').click();
+  })
+});
+
