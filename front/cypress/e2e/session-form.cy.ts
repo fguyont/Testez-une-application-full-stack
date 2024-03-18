e8524c110cce6aed89112a6a381d9179a0a34568
Frederic Guyont <you@example.com>
2024-03-18 23:44:24 +0100

    FIX: session creation e2e

--- front/cypress/e2e/session-form.cy.ts
+++ front/cypress/e2e/session-form.cy.ts
@@ -1,5 +1,5 @@
 describe('session form', () => {
-  it('shows session creation form', () => {
+  it('create new session', () => {
     cy.visit('/login');
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -16,7 +16,10 @@ describe('session form', () => {
         id: 1,
         name: "Beginner test session",
         teacher_id: 1,
-        description: "Session for test"
+        description: "Session for test",
+        users: [],
+        date: "2024-12-25T00:00:00",
+        createdAt: "2024-02-25T00:00:00"
       }]
     });
 
@@ -27,6 +30,55 @@ describe('session form', () => {
 
     cy.contains('Rentals');
 
+    let teachers = [{
+      id: 1,
+      lastName: "DELAHAYE",
+      firstName: "Margot"
+    },
+    {
+      id: 2,
+      lastName: "THIERCELIN",
+      firstName: "Hélène"
+    }];
+
+    cy.intercept('GET', '/api/teacher', teachers);
+
     cy.get('button[mat-raised-button]').contains('Create').click();
-  })
+
+    cy.intercept('GET', '/api/session', {
+      body: [{
+        id: 1,
+        name: "Beginner test session",
+        teacher_id: 1,
+        description: "Session for test",
+        users: [],
+        date: "2024-12-25T00:00:00",
+        createdAt: "2024-02-25T00:00:00"
+      },
+      {
+        id: 2,
+        name: "New session for experts",
+        teacher_id: 2,
+        description: 'New session for experts not noobs !',
+        users: [],
+        date: "2024-12-21T00:00:00",
+        createdAt: "2024-02-25T00:00:00"
+      }
+    ]
+    });
+
+    cy.intercept('POST', '/api/session', {
+      statusCode: 200
+    })
+
+    cy.get('input[formControlName=name]').type("New session for experts")
+    cy.get('input[formControlName="date"]').type('2024-12-21');
+    cy.get('mat-select[formControlName="teacher_id"]').click().get('mat-option').contains('THIERCELIN').click();
+    cy.get('textarea[formControlName="description"]').type('New session for experts not noobs !');
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/sessions');
+
+  });
+
 });
