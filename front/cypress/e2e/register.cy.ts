bafda215df5325a125b733b6a776d90e4807d18d
Frederic Guyont <you@example.com>
2024-03-18 23:43:17 +0100

    ADD: register error test

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -28,4 +28,19 @@ describe('Register spec', () => {
 
     cy.get('form > span').contains("error")
   })
+
+  it('Register failed with an invalid password with one character and error message appeared', () => {
+    cy.visit('/register')
+
+    cy.intercept('POST', '/api/auth/register', {
+      statusCode: 400
+    })
+
+    cy.get('input[formControlName=firstName]').type(`${""}{enter}`)
+    cy.get('input[formControlName=lastName]').type(`${""}{enter}`)
+    cy.get('input[formControlName=email]').type(`${""}{enter}`)
+    cy.get('input[formControlName=password]').type(`${""}{enter}{enter}`)
+
+    cy.get('form > span').contains("error")
+  })
 });
