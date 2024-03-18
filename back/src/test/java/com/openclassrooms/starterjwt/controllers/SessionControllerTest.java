80a489bf1ec43eaaf9a2baa29fc3e41e139685ba
Frederic Guyont <you@example.com>
2024-03-18 23:41:33 +0100

    FIX: SessionController tests with order

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
@@ -2,7 +2,10 @@ package com.openclassrooms.starterjwt.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.dto.SessionDto;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SessionControllerTest {
@@ -27,14 +31,16 @@ public class SessionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Order(3)
     @Test
     @WithMockUser
     public void whenGetSession_thenShouldGetSessionDetails() throws Exception {
-        mockMvc.perform(get("/api/session/2"))
+        mockMvc.perform(get("/api/session/1"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(2)));
+                .andExpect(jsonPath("$.id", is(1)));
     }
 
+    @Order(4)
     @Test
     @WithMockUser
     public void whenGetSession_thenShouldNotFoundSession() throws Exception {
@@ -42,6 +48,7 @@ public class SessionControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Order(5)
     @Test
     @WithMockUser
     public void whenGetSession_thenShouldBadRequest() throws Exception {
@@ -49,6 +56,7 @@ public class SessionControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Order(6)
     @Test
     @WithMockUser
     public void whenGetAllSessions_thenShouldFindAllSessions() throws Exception {
@@ -56,12 +64,14 @@ public class SessionControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Order(1)
     @Test
     public void whenGetAllSessionsWithoutBeingLogged_thenShouldUnauthorized() throws Exception {
         mockMvc.perform(get("/api/session"))
                 .andExpect(status().isUnauthorized());
     }
 
+    @Order(2)
     @Test
     @WithMockUser
     public void whenCreateASession_thenShouldCreateASession() throws Exception {
@@ -78,7 +88,7 @@ public class SessionControllerTest {
                 .andExpect(status().isOk());
     }
 
-
+    @Order(8)
     @Test
     @WithMockUser
     public void whenUpdateASession_thenShouldUpdateASession() throws Exception {
@@ -89,12 +99,13 @@ public class SessionControllerTest {
         sessionDto.setDescription("I repeat for beginners !");
         String jsonRequest = objectMapper.writeValueAsString(sessionDto);
 
-        mockMvc.perform(put("/api/session/3")
+        mockMvc.perform(put("/api/session/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRequest))
                 .andExpect(status().isOk());
     }
 
+    @Order(7)
     @Test
     @WithMockUser
     public void whenUpdateASessionWithoutAName_thenShouldBadRequest() throws Exception {
@@ -105,12 +116,21 @@ public class SessionControllerTest {
         sessionDto.setDescription("I repeat for beginners !");
         String jsonRequest = objectMapper.writeValueAsString(sessionDto);
 
-        mockMvc.perform(put("/api/session/3")
+        mockMvc.perform(put("/api/session/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonRequest))
                 .andExpect(status().isBadRequest());
     }
 
+    @Order(17)
+    @Test
+    @WithMockUser
+    public void whenDeleteASession_thenShouldDelete() throws Exception {
+        mockMvc.perform(delete("/api/session/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Order(16)
     @Test
     @WithMockUser
     public void whenDeleteASessionWithABadUrl_thenShouldBadRequest() throws Exception {
@@ -118,6 +138,7 @@ public class SessionControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Order(15)
     @Test
     @WithMockUser
     public void whenDeleteANonExistingSession_thenShouldNotFound() throws Exception {
@@ -125,45 +146,51 @@ public class SessionControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Order(9)
     @Test
     @WithMockUser
     public void whenParticipateASession_thenShouldParticipate() throws Exception {
-        mockMvc.perform(post("/api/session/2/participate/3"))
+        mockMvc.perform(post("/api/session/1/participate/1"))
                 .andExpect(status().isOk());
     }
 
+    @Order(10)
     @Test
     @WithMockUser
     public void whenParticipateASessionWithABadUrl_thenShouldBadRequest() throws Exception {
-        mockMvc.perform(post("/api/session/2/participate/teacher"))
+        mockMvc.perform(post("/api/session/1/participate/teacher"))
                 .andExpect(status().isBadRequest());
     }
 
+    @Order(13)
     @Test
     @WithMockUser
     public void whenParticipateASessionWithANonExistingSession_thenShouldNotFound() throws Exception {
-        mockMvc.perform(post("/api/session/-999/participate/2"))
+        mockMvc.perform(post("/api/session/-999/participate/1"))
                 .andExpect(status().isNotFound());
     }
 
+    @Order(14)
     @Test
     @WithMockUser
     public void whenNoLongerParticipateASession_thenShouldNoLongerParticipate() throws Exception {
-        mockMvc.perform(delete("/api/session/2/participate/3"))
+        mockMvc.perform(delete("/api/session/1/participate/1"))
                 .andExpect(status().isOk());
     }
 
+    @Order(12)
     @Test
     @WithMockUser
     public void whenNoLongerParticipateASessionWithABadUrl_thenShouldBadRequest() throws Exception {
-        mockMvc.perform(delete("/api/session/2/participate/teacher"))
+        mockMvc.perform(delete("/api/session/1/participate/teacher"))
                 .andExpect(status().isBadRequest());
     }
 
+    @Order(11)
     @Test
     @WithMockUser
     public void whenNoLongerParticipateASessionWithANonExistingSession_thenShouldNotFound() throws Exception {
-        mockMvc.perform(delete("/api/session/-999/participate/2"))
+        mockMvc.perform(delete("/api/session/-999/participate/1"))
                 .andExpect(status().isNotFound());
     }
 
