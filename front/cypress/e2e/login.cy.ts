b61c3f5535d90af573fe958b58869bf98db5b8be
Frederic Guyont <you@example.com>
2024-03-18 23:42:48 +0100

    ADD: login error test

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -24,4 +24,75 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('Login failed with bad credentials', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"badtest!1234"}{enter}{enter}`)
+
+    cy.get('form > p').contains("error")
+  })
+
+  it('Login failed with empty inputs', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 400
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type(`${""}{enter}`)
+    cy.get('input[formControlName=password]').type(`${""}{enter}{enter}`)
+
+    cy.get('form > p').contains("error")
+  })
+
+  it('Logout successfull', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/sessions')
+
+    cy.contains('Logout').click();
+
+    cy.url().should('include', '/');
+
+  })
 });
